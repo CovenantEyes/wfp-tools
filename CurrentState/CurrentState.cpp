8d3b57962ce7fd4baeafe7908973b1990100d0ab
3noch <elliot@3noch.com>
2012-05-25 12:21:39 -0400

    Trying to enumerate all layers.

--- CurrentState/CurrentState.cpp
+++ CurrentState/CurrentState.cpp
@@ -5,107 +5,123 @@
 #pragma comment (lib, "fwpuclnt.lib")
 
 #define EXIT_ON_ERROR(fnName) \
-	if (result != ERROR_SUCCESS) \
-   { \
-   printf(#fnName " = 0x%08X\n", result); \
-   goto CLEANUP; \
-   }
+    if (result != ERROR_SUCCESS) \
+    { \
+        printf(#fnName " = 0x%08X\n", result); \
+        goto CLEANUP; \
+    }
 
-DWORD FindMatchingFilters(
+DWORD GetAllLayers(
 	__in HANDLE engine,
-	__in const GUID* layerKey,
-	__in_opt PCWSTR appPath,
-	__in_opt const SOCKADDR* localAddr,
-	__in_opt UINT8 ipProtocol,
-	__deref_out_ecount(*numFilters) FWPM_FILTER0*** filters,
-	__out UINT32* numFilters
-	)
+	__deref_out_ecount(*layerCount) FWPM_LAYER *** layers,
+	__out UINT32 * layerCount)
 {
 	DWORD result = ERROR_SUCCESS;
-	FWPM_FILTER_CONDITION0 conds[4];
-	UINT32 numConds = 0;
-	FWP_BYTE_BLOB* appBlob = NULL;
-	FWPM_FILTER_ENUM_TEMPLATE0 enumTempl;
+
+	HANDLE enumHandle;
+	result = FwpmLayerCreateEnumHandle(
+		engine,
+		NULL, // Don't filter out any layers (no enumeration template).
+		&enumHandle);
+	EXIT_ON_ERROR(FwpmLayerCreateEnumHandle);
+
+	FwpmLayerEnum(
+		engine,
+		enumHandle,
+		INFINITE, // Don't limit how many layers we get back.
+		layers,
+		layerCount);
+	EXIT_ON_ERROR(FwpmLayerCreateEnumHandle);
+	
+CLEANUP:
+	FwpmLayerDestroyEnumHandle(engine, enumHandle);
+	return result;
+}
+
+DWORD GetAllFiltersInLayer(
+	__in HANDLE engine,
+	__in const GUID * layerKey,
+	__deref_out_ecount(*filterCount) FWPM_FILTER *** filters,
+	__out UINT32 * filterCount)
+{
+	DWORD result = ERROR_SUCCESS;
+	FWPM_FILTER_ENUM_TEMPLATE enumTemplate;
 	HANDLE enumHandle = NULL;
 
-	memset(&enumTempl, 0, sizeof(enumTempl));
-	enumTempl.layerKey = *layerKey;
-	enumTempl.numFilterConditions = numConds;
-	if (numConds > 0)
-	{
-		enumTempl.filterCondition = conds;
-	}
-	// We want to see all filters regardless of action.
-	enumTempl.actionMask = 0xFFFFFFFF;
+	memset(&enumTemplate, 0, sizeof(enumTemplate));
+	enumTemplate.layerKey = *layerKey;    // Only select filters in the layer with `layerKey`.
+	enumTemplate.numFilterConditions = 0;
+	enumTemplate.actionMask = 0xFFFFFFFF; // We want to see all filters regardless of action.
 
-	result = FwpmFilterCreateEnumHandle0(
+	result = FwpmFilterCreateEnumHandle(
 		engine,
-		&enumTempl,
-		&enumHandle
-		);
-	EXIT_ON_ERROR(FwpmFilterCreateEnumHandle0);
+		&enumTemplate,
+		&enumHandle);
+	EXIT_ON_ERROR(FwpmFilterCreateEnumHandle);
 
-	result = FwpmFilterEnum0(
+	result = FwpmFilterEnum(
 		engine,
 		enumHandle,
 		INFINITE,
 		filters,
-		numFilters
-		);
-	EXIT_ON_ERROR(FwpmFilterEnum0);
+		filterCount);
+	EXIT_ON_ERROR(FwpmFilterEnum);
 
 CLEANUP:
-	FwpmFilterDestroyEnumHandle0(engine, enumHandle);
-	FwpmFreeMemory0((void**)&appBlob);
+	FwpmFilterDestroyEnumHandle(engine, enumHandle);
 	return result;
 }
 
-DWORD wmain(int argc,
-	wchar_t* argv[])
+DWORD wmain(void)
 {
-	UNREFERENCED_PARAMETER(argc);
-	UNREFERENCED_PARAMETER(argv);
-
-	// Open a session to the filter engine
-	HANDLE engineHandle = 0;
+	DWORD result = ERROR_SUCCESS;
 
 	// Use dynamic sessions for efficiency and safety:
 	//  - All objects associated with the dynamic session are deleted with one call.
-	//  - Filtering policy objects are deleted even when the application crashes. 
-	FWPM_SESSION0 session;
+	//  - Filtering policy objects are deleted even when the application crashes.
+	HANDLE engineHandle = 0;
+	FWPM_SESSION session;
 	memset(&session, 0, sizeof(session));
 	session.flags = FWPM_SESSION_FLAG_DYNAMIC;
 
-	UINT32 numFilters = 0;
-	FWPM_FILTER0** filters = 0;
-	DWORD result = FwpmEngineOpen0(NULL, RPC_C_AUTHN_WINNT, NULL, &session, &engineHandle);
-	EXIT_ON_ERROR(FwpmEngineOpen0);
+	result = FwpmEngineOpen(NULL, RPC_C_AUTHN_WINNT, NULL, &session, &engineHandle);
+	EXIT_ON_ERROR(FwpmEngineOpen);
 
-	result = FindMatchingFilters(
-		engineHandle,
-		&FWPM_LAYER_STREAM_V4,
-		0,
-		0,
-		0,
-		&filters,
-		&numFilters
-		);
+	UINT32 layerCount = 0;
+	FWPM_LAYER ** layers = 0;
+	result = GetAllLayers(engineHandle, &layers, &layerCount);
+	EXIT_ON_ERROR(GetAllLayers);
 
-CLEANUP:  
-	if (result != ERROR_SUCCESS)
+	for (size_t i = 0; i < layerCount; i++)
 	{
-		printf("Error: %x\n", result);
-	}
-	else
-	{
-		for (unsigned int i = 0; i < numFilters; i++)
+		UINT32 filterCount = 0;
+		FWPM_FILTER ** filters = 0;
+
+		result = GetAllFiltersInLayer(
+			engineHandle,
+			&(layers[i]->layerKey),
+			&filters,
+			&filterCount);
+
+		if (result == ERROR_SUCCESS)
 		{
-			printf("\n%d. %ws", i + 1, (filters[i])->displayData.name);
+			printf("\n%ws: %d filters", layers[i]->displayData.name, filterCount);
+
+			for (size_t j = 0; j < filterCount; j++)
+			{
+				printf("\n\t%d.\t%ws (%ws)", j + 1, filters[j]->displayData.name, filters[j]->displayData.description);
+			}
 		}
-		printf("\nSuccess: %d filters", numFilters);
+		else
+		{
+			printf("\n\tError getting filters (error code 0x%08X)", result);
+		}
+
+		FwpmFreeMemory((void**)filters);
 	}
 
-	FwpmFreeMemory0((void**)filters);
-
+CLEANUP:
+	FwpmFreeMemory((void**)layers);
+	FwpmEngineClose(engineHandle); // This is automatically done when the program closes, but I want to be explicit.
 	return result;
 }
